8c1ae82319dbe3a25b057eff0d9d812382ddbc82
Vishal Vishwakarma <vishwakarmavishal14@gmail.com>
2024-05-19 21:40:55 +0530

    Multiple Major changes
    Multiple Major changes

--- js/api.js
+++ js/api.js
@@ -1,3 +1,17 @@
+/*!
+=========================================================
+* iamvisshu Landing page
+=========================================================
+
+* Copyright: 2024 iamvisshu (https://github.com/iamvisshu)
+* Coded by https://github.com/iamvisshu
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+
 async function fetchData() {
     try {
         const response = await fetch('https://services.isrostats.in/api/launches');
